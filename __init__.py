33b7cf9454225486f8da07ce795e74b3c2fb9173
Randy Hucker <105221281+RandyHucker@users.noreply.github.com>
2022-09-26 10:23:32 -0400

    Update __init__.py
    Co-authored-by: Craig Vermeer <89785132+craigv-physna@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -402,7 +402,8 @@ class ThangsLink(bpy.types.Operator):
 
     def execute(self, context):
         amplitude.send_amplitude_event("nav to thangs", event_properties={
-                                       'device_os': str(fetcher.devideOS), 'device_ver': str(fetcher.deviceVer), 'source': "Blender Addon"})
+                                       'device_os': str(fetcher.devideOS), 'device_ver': str(fetcher.deviceVer), 'source': "blender"})
+
         webbrowser.open("https://thangs.com/search/"+fetcher.query +
                         "?utm_source=blender&utm_medium=referral&utm_campaign=blender_extender&fileTypes=stl%2Cgltf%2Cobj%2Cfbx%2Cglb%2Csldprt%2Cstep%2Cmtl%2Cdxf%2Cstp&scope=thangs", new=0, autoraise=True)
         return {'FINISHED'}
