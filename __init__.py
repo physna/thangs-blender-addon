f80db6f94abbcf868b34e74500f070e9a4af37a6
RandyHucker <rhucker@physna.com>
2022-09-27 13:17:32 -0400

    Added Property

--- __init__.py
+++ __init__.py
@@ -735,7 +735,7 @@ def open_timer():
                     n_panel_is_open = space.show_region_ui
 
     amplitude.send_amplitude_event("Thangs Blender Addon - Opened", event_properties={'device_os': str(
-        fetcher.devideOS), 'device_ver': str(fetcher.deviceVer), 'source': "blender"})
+        fetcher.devideOS), 'device_ver': str(fetcher.deviceVer), 'source': "blender", 'panel_open': n_panel_is_open})
     return 60
 
 def register():
