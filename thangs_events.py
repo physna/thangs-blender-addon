ba3205b6b5d4d0d583afb85dea863f15bbfa415f
Randy Hucker <105221281+RandyHucker@users.noreply.github.com>
2022-09-28 14:20:42 -0400

    Update thangs_events.py

--- thangs_events.py
+++ thangs_events.py
@@ -7,7 +7,6 @@ import time
 import logging
 import os
 
-# __location__ = os.path.realpath(
 # config_file = open(os.path.join(__location__, 'configfile.ini'))
 
 config_obj = configparser.ConfigParser(allow_no_value=True)
