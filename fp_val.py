1001517cfd8cab7a8d960a901f728837114b0bff
jmontanezphysna <jmontanez@physna.com>
2022-10-01 13:11:24 -0400

    Ranamed module

--- fp_val.py
+++ fp_val.py
@@ -5,8 +5,8 @@ class FP:
 
         if FP.val == "":
             import requests
-            from .machine_Id import MachineID
-
+            from .machine_id import MachineID
+            
             machineID = MachineID().getID()
             response = requests.post(url, json={ 'machineID': machineID })
             FP.val = response.text
