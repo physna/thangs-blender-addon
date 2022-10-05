ddfc523bb1ef94dae7f850636f50a7bb1638932c
jmontanezphysna <jmontanez@physna.com>
2022-10-05 16:12:38 -0400

    Add explanatory comments

--- fp_val.py
+++ fp_val.py
@@ -7,11 +7,13 @@ class FP:
             import requests
             from .machine_id import MachineID
             
+            # Acquire stable identifier for API requests metrics. 
             machineID = MachineID().getID()
             try: 
+                # Post identifier to backend for aggregation
                 response = requests.post(url, json={ 'machineID': machineID })
                 FP.val = response.text
-                return FP.val
+                return FP.val # Return identifier for request correlation
             except:
                 return FP.val
     
