fcb1f79289da944263dbd9d08a04980e6531bf77
Randy Hucker <105221281+RandyHucker@users.noreply.github.com>
2022-11-21 14:52:34 -0500

    Update thangs_importer.py

--- thangs_importer.py
+++ thangs_importer.py
@@ -306,7 +306,7 @@ class ThangsApi:
                     'domain': self.model.domain,
                     'success': False,
                     'exception': e,
-                  })
+                })
             return
         
         self.amplitude.send_amplitude_event("Thangs Blender Addon - import model", event_properties={
