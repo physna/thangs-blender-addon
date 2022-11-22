08ea0ac9f6118d7f72f50b921989fb6b52c897b9
Craig Vermeer <cvermeer@physna.com>
2022-11-22 10:04:48 -0500

    0.2.8

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ log = logging.getLogger(__name__)
 bl_info = {
     "name": "Thangs Model Search",
     "author": "Thangs",
-    "version": (0, 2, 7),
+    "version": (0, 2, 8),
     "blender": (3, 2, 0),
     "location": "VIEW 3D > Tools > Thangs Search",
     "description": "Browse and import free 3D models",
