02deadf3dbcbfd5e9bf13db50aef48f349623494
garnos <102166356+garnos@users.noreply.github.com>
2023-07-13 17:27:33 -0400

    Merge back main into dev for blender-addon (#128)
    * Updating README with installation instruction improvements
    
    * Adding in Blender Market and Gumroad
    
    * Flip to ordered list
    
    * Fixed some bugs with sending failure metrics.  Also added metrics around downloads.
    
    * Fixed typos, fixed exception serialization
    
    * Version bump
    
    * Release - version bump
    
    * update readme for Sync feature
    
    * Wordsmithing
    
    * Grammar
    
    * cleanup
    
    * Version bump.
    
    ---------
    
    Co-authored-by: Adam Gleichsner <agleichsner@physna.com>
    Co-authored-by: River Gleichsner <80697933+gleichsnerd-physna@users.noreply.github.com>
    Co-authored-by: Brenden Sharp <bsharp@physna.com>
    Co-authored-by: Craig Vermeer <cvermeer@physna.com>
    Co-authored-by: Craig Vermeer <89785132+craigv-physna@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@
 bl_info = {
     "name": "Thangs",
     "author": "Thangs",
-    "version": (0, 2, 9),
+    "version": (0, 3, 1),
     "blender": (3, 2, 0),
-    "location": "VIEW 3D > Tools > Thangs Search",
-    "description": "Browse and import free 3D models",
+    "location": "VIEW 3D > Tools > Thangs",
+    "description": "Browse, import, and upload 3D models",
     "warning": "",
     "support": "COMMUNITY",
     "wiki_url": "https://github.com/physna/thangs-blender-addon",
